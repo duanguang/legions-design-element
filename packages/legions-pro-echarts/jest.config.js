e7df9e9e7c3ee187709cdc1484cf5e5db68e5d21
duanguang <duanguang@hoolinks.com>
2020-12-22 18:07:59 +0800

    test: 添加饼图单元测试用例

--- packages/legions-pro-echarts/jest.config.js
+++ packages/legions-pro-echarts/jest.config.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: duanguang
+ * @Date: 2020-12-09 15:43:49
+ * @LastEditTime: 2020-12-22 17:31:41
+ * @LastEditors: duanguang
+ * @Description: 
+ * @FilePath: /legions-design-element/packages/legions-pro-echarts/jest.config.js
+ * @「扫去窗上的尘埃，才可以看到窗外的美景。」
+ */
 module.exports = {
   preset: 'ts-jest',
   // 测试目录
-  roots: ['<rootDir>/__tests__'],
+  roots: ['<rootDir>/__tests__','<rootDir>/src/components'],
   // 对 ts tsx 文件使用 ts-jest 进行运行测试
   transform: {
     '.(ts|tsx)': 'ts-jest',
@@ -22,6 +31,12 @@ module.exports = {
       statements: 95,
     }, */
   },
+  "testEnvironment": "jsdom",
+  "setupFiles": [
+    "./script/setup.ts",
+    "jest-canvas-mock",
+    /* "jsdom-global/register" */
+  ],
   globals: {
     'ts-jest': {
       diagnostics: {
