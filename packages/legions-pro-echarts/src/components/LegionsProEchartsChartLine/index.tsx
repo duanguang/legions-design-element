f9bd594064ec5f5898085791541512f9a8e0d7fb
guoxiaojun <xiaojun_guo2019@163.com>
2020-12-29 17:17:20 +0800

    fix: 折线组件重写ready函数时，绑定组件实例

--- packages/legions-pro-echarts/src/components/LegionsProEchartsChartLine/index.tsx
+++ packages/legions-pro-echarts/src/components/LegionsProEchartsChartLine/index.tsx
@@ -148,6 +148,7 @@ export class LegionsProEchartsChartLine extends React.Component<LegionsProEchart
                 if(this.props.onChartReady){
                     //@ts-ignore
                     this.props.onChartReady({
+                        ...value,
                         methods:{
                             onSearch:(option?:Object)=>{
                                 this.getData(option)
