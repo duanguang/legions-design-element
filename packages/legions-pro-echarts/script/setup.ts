ceafaf77f6d6d2b46d355ec5508c1383ca3b5f0a
duanguang <duanguang@hoolinks.com>
2021-01-04 16:26:42 +0800

    test(legions-pro-echarts): 修复单元测试无法正常运行问题

--- packages/legions-pro-echarts/script/setup.ts
+++ packages/legions-pro-echarts/script/setup.ts
@@ -1,13 +1,13 @@
 /*
  * @Author: duanguang
  * @Date: 2020-12-22 16:33:23
- * @LastEditTime: 2020-12-22 17:02:53
+ * @LastEditTime: 2021-01-04 16:24:43
  * @LastEditors: duanguang
  * @Description: 
- * @FilePath: /legions-design-element/packages/legions-pro-echarts/__tests__/setup.ts
+ * @FilePath: /legions-design-element/packages/legions-pro-echarts/script/setup.ts
  * @「扫去窗上的尘埃，才可以看到窗外的美景。」
  */
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
 
 // refs only work with mount, yes.
